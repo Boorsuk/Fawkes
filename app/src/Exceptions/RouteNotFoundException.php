0726b18bdd0d9fe249593f5dc7d6089cfbeae217
patryk <patzaj9711@gmail.com>
2022-06-12 16:40:22 +0200

    Now RouteNotFoundException extends \Exception

--- app/src/Exceptions/RouteNotFoundException.php
+++ app/src/Exceptions/RouteNotFoundException.php
@@ -4,9 +4,7 @@ declare(strict_types = 1);
 
 namespace Fawkes\Exceptions;
 
-use Exception;
-
-class RouteNotFoundException extends Exception
+class RouteNotFoundException extends \Exception
 {
     protected $message = 'Route not found';
 }
