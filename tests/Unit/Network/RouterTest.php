50ad9b6abb4f2648965716402aab75b7f9eb536c
patryk <patzaj9711@gmail.com>
2022-06-17 12:10:37 +0200

    Moved router test to catalog Network

--- tests/Unit/Network/RouterTest.php
+++ tests/Unit/Network/RouterTest.php
@@ -2,8 +2,9 @@
 
 declare(strict_types = 1);
 
-namespace Tests\Unit;
+namespace Tests\Network\Unit;
 
+use Fawkes\Container;
 use Fawkes\Network\Router;
 use PHPUnit\Framework\TestCase;
 
@@ -14,6 +15,8 @@ class RouterTest extends TestCase
      * @test 
      */
     public function should_return_empty_routes_on_init(){
-        $this->assertEmpty((new Router)->routes());
+        $container = Container::init();
+        
+        $this->assertEmpty((new Router($container))->routes());
     }
 }
